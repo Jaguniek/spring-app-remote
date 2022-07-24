9abdfa335c574c848e22e0b5be16547448ca2347
Wojciech Szambelan <suchywojtas@gmail.com>
2022-07-25 00:04:05 +0200

    feat: DIGIHUB-123 integration test

--- application/src/test/java/application/ConstraintsIntegrationTest.java
+++ application/src/test/java/application/ConstraintsIntegrationTest.java
@@ -1,5 +1,23 @@
 package application;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.jaguniek.SpringAppApplication;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = { SpringAppApplication.class }, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ConstraintsIntegrationTest {
-//TODO: implement integration test
+
+	//@MockBean
+	//private ExternalProductService/Database service/database;
+
+	 //@Test
+	 //void checkIfAllowedTest() {
+	
+	//TODO: implement integration test
+
+	//}
 }
